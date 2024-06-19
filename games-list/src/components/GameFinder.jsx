59964940ecc243609d56dd56a812dda534b37b65
[Adarsh Menon] <[s2088639]@sms.ed.ac.uk>
2024-06-19 14:12:30 +0100

    GamesFinder updated removing api key

--- games-list/src/components/GameFinder.jsx
+++ games-list/src/components/GameFinder.jsx
@@ -1,8 +1,7 @@
 export default function GameFinder() {
   async function fetchAPI() {
-    const response = await fetch(
-      "https://api.rawg.io/api/games?key=d31d87807c3c4f329ea4371eaa899a20"
-    );
+    var key = "d31d87807c3c4f329ea4371eaa899a20";
+    const response = await fetch("https://api.rawg.io/api/games?key=" + key);
     const data = await response.json();
     console.log(data.results[0].playtime);
     return "HI";
