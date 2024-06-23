e0dfc224fd85cd47814e0487475a5cb0e57f2237
[Adarsh Menon] <[s2088639]@sms.ed.ac.uk>
2024-06-23 18:29:22 +0100

    changed styling and added shadows of modal

--- games-list/src/components/Header.tsx
+++ games-list/src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <h1>Conquest 🛡</h1>
+      <h1>Conquest</h1>
     </div>
   );
 }
