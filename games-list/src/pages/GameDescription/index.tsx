f12b0811e7ed39170b4699ce912199c6a8120b1a
[Adarsh Menon] <[s2088639]@sms.ed.ac.uk>
2024-06-23 23:19:29 +0100

    Added more css, and introducing new page to view more game details

---
+++ games-list/src/pages/GameDescription/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function GameDescription() {
+  return (
+    <main>
+      <h1>HI</h1>
+    </main>
+  );
+}
